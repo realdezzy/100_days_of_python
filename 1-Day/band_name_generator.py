bcf93c33571d815a53406c0a155c097daae5c170
Dezzy <ikeogufranklin1@gmail.com>
2022-12-06 16:58:47 +0100

    Band name

---
+++ 1-Day/band_name_generator.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+#Greeting
+
+print("Welcome to Band Name Generator App! \n")
+
+#Collect data from user prompt
+city = input("What's the name of your city? \n")
+pet = input("What's your pet name? \n")
+
+#Print band name
+print("Your band name is {} {}".format(city, pet))
